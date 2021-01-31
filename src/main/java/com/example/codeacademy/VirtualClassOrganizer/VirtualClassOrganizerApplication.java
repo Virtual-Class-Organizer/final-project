6bb2060a54bac0ed21cb835836f9f1d77bdaae51
Borjan <brainstererrors@gmail.com>
2021-01-31 11:41:27 +0100

    Setting Database Configuration

--- src/main/java/com/example/codeacademy/VirtualClassOrganizer/VirtualClassOrganizerApplication.java
+++ src/main/java/com/example/codeacademy/VirtualClassOrganizer/VirtualClassOrganizerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VirtualClassOrganizerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VirtualClassOrganizerApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
